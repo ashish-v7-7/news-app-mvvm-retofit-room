cb536ede87bbfec6adaa1ee8fe48ef42b78b0814
ZaidZakir <zaidzakir@hlbb.hongleong.com.my>
2021-04-25 13:07:44 +0800

    show articles in webview

--- app/src/main/java/com/zaidzakir/newsapp/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/zaidzakir/newsapp/ui/fragments/ArticleFragment.kt
@@ -2,10 +2,13 @@ package com.zaidzakir.newsapp.ui.fragments
 
 import android.os.Bundle
 import android.view.View
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.zaidzakir.newsapp.R
 import com.zaidzakir.newsapp.ui.NewsActivity
 import com.zaidzakir.newsapp.ui.NewsViewModel
+import kotlinx.android.synthetic.main.fragment_article.*
 
 /**
  *Created by Zaid Zakir
@@ -13,9 +16,15 @@ import com.zaidzakir.newsapp.ui.NewsViewModel
 class ArticleFragment:Fragment(R.layout.fragment_article) {
 
     lateinit var viewModel: NewsViewModel
+    val args: ArticleFragmentArgs by navArgs()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as NewsActivity).viewModel
+        val article = args.article
+        webView.apply {
+            webViewClient = WebViewClient()
+            loadUrl(article.url)
+        }
     }
 }
